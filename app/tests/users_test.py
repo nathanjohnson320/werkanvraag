90e5297ed33331b8b7263aa4f2ea8349ac2d1817
Nathan Johnson <nathanjohnson320@gmail.com>
2024-07-26 22:21:57 -0400

    Test login

--- app/tests/users_test.py
+++ app/tests/users_test.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
-from app.tests.db import app
+from app.tests.db import app, override_get_db
+from app.contexts import users
+from app.schemas import UserCreate
 
 client = TestClient(app)
 
@@ -13,3 +15,21 @@ def test_create_user():
     data = response.json()
     assert data["email"] == "deadpool@example.com"
     assert "id" in data
+
+
+def test_login_user():
+    email = "deadpool@example.com"
+    password = "chimichangas4life"
+
+    response = client.post(
+        "/api/v1/users",
+        json={"email": email, "password": password},
+    )
+
+    response = client.post(
+        "/api/v1/users/login",
+        json={"email": email, "password": password},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["email"] == email
